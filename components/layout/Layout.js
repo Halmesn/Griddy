437718ef4d23b8048daa81817ac80fa50bab02b6
Adrian Li <xiaxi.li.syd@gmail.com>
2021-06-01 12:13:13 +1000

    add useWindowWidth custom hook

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -3,14 +3,24 @@ import { darkTheme, lightTheme } from 'styles/Themes';
 import { GlobalStyles } from 'styles/GlobalStyles';
 
 import { useDarkMode } from 'hooks/useDarkMode';
+import { useWindowWidth } from 'hooks/useWindowWidth';
+
+import { createContext } from 'react';
+import Header from './header/Header';
+
+export const GriddyContext = createContext();
 
 export default function Layout({ children }) {
   const [theme, themeToggler] = useDarkMode();
+  const width = useWindowWidth();
 
   return (
     <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
       <GlobalStyles />
-      <main>{children}</main>
+      <GriddyContext.Provider value={{ theme, themeToggler, width }}>
+        <Header />
+        <main>{children}</main>
+      </GriddyContext.Provider>
     </ThemeProvider>
   );
 }
