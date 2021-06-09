af45ef0ac2ac3b9cc5b68d2008bd7c4a6d3ce72a
Adrian Li <xiaxi.li.syd@gmail.com>
2021-06-09 12:14:51 +1000

    add quiz 2 answer check to Results component

--- components/quiz/Results.js
+++ components/quiz/Results.js
@@ -5,12 +5,29 @@ import { QUIZ } from 'content/quizContent';
 export default function Results({ answers }) {
   const { correctAnswers } = QUIZ;
 
-  const checkUserAnswer = () => {};
+  const isGridAreaCorrect = () => {
+    const gridAreaCorrect = correctAnswers[1];
+    const { line1, line2, line3 } = answers[1] ?? {
+      line1: '',
+      line2: '',
+      line3: '',
+    };
+
+    if (
+      line1.trim().toLowerCase() === gridAreaCorrect.line1 &&
+      line2.trim().toLowerCase() === gridAreaCorrect.line2 &&
+      line3.trim().toLowerCase() === gridAreaCorrect.line3
+    )
+      return true;
+    return false;
+  };
 
   const calculateScore = () => {
     let score = 0;
-    for (let i = 0; i < answers.length; i++)
-      if (answers[i] === correctAnswers[i]) score += 1;
+    for (let i = 0; i < answers.length; i++) {
+      if (i === 1 && isGridAreaCorrect()) score += 1;
+      else if (answers[i] === correctAnswers[i]) score += 1;
+    }
     return score;
   };
 
